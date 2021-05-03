d2588b25a09ea69abd7af33a906f41e03a72a24f
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-03 20:59:09 +0900

    Feat: 숫자, 연산 버튼 클릭 이벤트 연결, TextView 연결

--- app/src/main/java/lilcode/aop/p2/c04/calculator/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p2/c04/calculator/MainActivity.kt
@@ -3,28 +3,62 @@ package lilcode.aop.p2.c04.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+
+    private val expressionTextView: TextView by lazy {
+        findViewById<TextView>(R.id.expressionTextView)
+    }
+
+    private val resultTextView: TextView by lazy {
+        findViewById<TextView>(R.id.resultTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // 다크 테마 NO
         setContentView(R.layout.activity_main)
     }
 
-    fun buttonClicked(v: View){
+    fun buttonClicked(v: View) {
+        when (v.id) {
+            R.id.button0 -> numberButtonClicked("0")
+            R.id.button1 -> numberButtonClicked("1")
+            R.id.button2 -> numberButtonClicked("2")
+            R.id.button3 -> numberButtonClicked("3")
+            R.id.button4 -> numberButtonClicked("4")
+            R.id.button5 -> numberButtonClicked("5")
+            R.id.button6 -> numberButtonClicked("6")
+            R.id.button7 -> numberButtonClicked("7")
+            R.id.button8 -> numberButtonClicked("8")
+            R.id.button9 -> numberButtonClicked("9")
+            R.id.buttonPlus -> operatorButtonClicked("+")
+            R.id.buttonMinus -> operatorButtonClicked("-")
+            R.id.buttonMulti -> operatorButtonClicked("*")
+            R.id.buttonDivide -> operatorButtonClicked("/")
+            R.id.buttonModulo -> operatorButtonClicked("%")
+        }
+    }
+
+    private fun numberButtonClicked(number: String) {
 
     }
 
-    fun resultButtonClicked(v: View){
+    private fun operatorButtonClicked(operator: String) {
 
     }
 
-    fun historyButtonClicked(v: View){
+    fun resultButtonClicked(v: View) {
 
     }
 
-    fun clearButtonClicked(v: View){
+    fun historyButtonClicked(v: View) {
+
+    }
+
+    fun clearButtonClicked(v: View) {
 
     }
 }
