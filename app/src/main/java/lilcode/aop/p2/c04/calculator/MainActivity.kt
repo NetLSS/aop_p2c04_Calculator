29d88149cceb8295ed16fdceabd955dc44c47a80
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-03 18:43:56 +0900

    Feat: 기본 적인 버튼 구성 및 디자인

--- app/src/main/java/lilcode/aop/p2/c04/calculator/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p2/c04/calculator/MainActivity.kt
@@ -2,10 +2,29 @@ package lilcode.aop.p2.c04.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // 다크 테마 NO
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v: View){
+
+    }
+
+    fun resultButtonClicked(v: View){
+
+    }
+
+    fun historyButtonClicked(v: View){
+
+    }
+
+    fun clearButtonClicked(v: View){
+
     }
 }
