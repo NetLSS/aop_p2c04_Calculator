e8d3fc24b15604a57927766a61bda43d247448c7
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-05 13:04:21 +0900

    Feat: 히스토리뷰 열 때 모든 DB 값 내용 가져와서 붙이기 (LayoutInflater)

--- app/src/main/java/lilcode/aop/p2/c04/calculator/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p2/c04/calculator/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
@@ -201,8 +203,27 @@ class MainActivity : AppCompatActivity() {
 
         historyLayout.isVisible = true
 
-        // TODO: 디비에서 모든 기록 가져오기
-        // TODO: 뷰에 모든 기록 할당하기
+        historyLinearLayout.removeAllViews() // 리니어 레이아웃 하위에 있는 모든 뷰 삭제
+
+        // 디비에서 모든 기록 가져오기
+        // 뷰에 모든 기록 할당하기
+        Thread(Runnable {
+            db.historyDao().getAll().reversed().forEach {
+                // 뷰 생성하여 넣어주기
+                // 레이아웃 인플레이터 기능 사용 해보기
+                // ui 스레드 열기
+                runOnUiThread {
+                    // R.layout.history_row 에서 인플레이트를 시킴., root랑 attachToRoot. 나중에 addview를 통해 붙일거라 null, false
+                    val historyView =
+                        LayoutInflater.from(this).inflate(R.layout.history_row, null, false)
+                    historyView.findViewById<TextView>(R.id.expressionTextView).text = it.expression
+                    historyView.findViewById<TextView>(R.id.resultTextView).text = "= ${it.result}"
+
+                    historyLinearLayout.addView(historyView) // 뷰 추가
+                }
+            } // 리스트 뒤집어서 가져오기
+        }).start()
+
     }
 
     fun closeHistoryButtonClicked(v: View){
