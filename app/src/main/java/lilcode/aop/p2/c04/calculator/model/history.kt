fb5676ecedf0164377566402928425d96a606ccb
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-05 12:28:21 +0900

    Feat: room 에서 사용할 Db 테이블 정의

--- app/src/main/java/lilcode/aop/p2/c04/calculator/model/history.kt
+++ app/src/main/java/lilcode/aop/p2/c04/calculator/model/history.kt
@@ -1,11 +1,13 @@
 package lilcode.aop.p2.c04.calculator.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity // room 사용
+import androidx.room.PrimaryKey
 
 // 데이터 클래스로 선언
-@Entity // room 사용
+@Entity // room 사용(room의 데이터 클래스) db 테이블
 data class History(
-    val uid: Int?, // 유니크한 아이디
-    val expression: String?,
-    val result: String?
+    @PrimaryKey val uid: Int?, // 유니크한 아이디 (주요키로 사용되는 듯)
+    @ColumnInfo(name = "expression") val expression: String?,
+    @ColumnInfo(name = "result") val result: String?
 )
