1535d21ac81ba03d1e86a8869a2a63b4259bdcd1
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-05 12:36:15 +0900

    Feat: 삽입, 전체삭제 쿼리 작성 (+a)

--- app/src/main/java/lilcode/aop/p2/c04/calculator/dao/HistoryDao.kt
+++ app/src/main/java/lilcode/aop/p2/c04/calculator/dao/HistoryDao.kt
@@ -1,6 +1,8 @@
 package lilcode.aop.p2.c04.calculator.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 import lilcode.aop.p2.c04.calculator.model.History
 
@@ -12,6 +14,16 @@ interface HistoryDao {
     @Query("SELECT * FROM history") // 쿼리문 작성
     fun getAll(): List<History>
 
+    @Insert
+    fun insertHistory(history: History)
 
+    @Query("DELETE FROM history") // 테이블 전체 삭제
+    fun deleteAll()
 
+    @Delete // 해당 히스트로리만 제거
+    fun delete(history: History)
+
+    // 조건 가진 결과만 가져오기 (1개만)
+    @Query("SELECT * FROM History WHERE result LIKE :result LIMIT 1")
+    fun findByResult(result: String): History
 }
